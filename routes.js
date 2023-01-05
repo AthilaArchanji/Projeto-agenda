816f7776de19043d9e76b4a4a7d93ba12b5c4660
Athila <athila.rodriguess@gmail.com>
2023-01-05 15:34:30 -0300

    creat, read, update e delete feitos. O fim do CRUd :rocket:

--- routes.js
+++ routes.js
@@ -3,6 +3,8 @@ const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
+const { loginRequired } = require('./src/middlewares/middleware');
 
 // Rotas da home
 route.get('/', homeController.index);
@@ -11,6 +13,12 @@ route.get('/', homeController.index);
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
 route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
 
-
+//rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
 module.exports = route;
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -25,6 +25,7 @@ class Login {
       this.errors.push('Usuário não existe.');
       return;
     }
+
     if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
       this.errors.push('Senha inválida');
       this.user = null;
@@ -34,43 +35,43 @@ class Login {
 
   async register() {
     this.valida();
-    if (this.errors.length > 0) return;
+    if(this.errors.length > 0) return;
 
-    //Impedindo de cadastrar mais de um usuario
     await this.userExists();
 
-    if (this.errors.length > 0) return;
+    if(this.errors.length > 0) return;
 
-    //Encriptando a senha do usuario
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
     this.user = await LoginModel.create(this.body);
   }
+
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email });
-
-    if (user) this.errors.push('Usuário já existe.');
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if(this.user) this.errors.push('Usuário já existe.');
   }
-  valida() {
-    //Primeiro chamamos a função cleanUp() para limpar o campo 
-    //e termos certeza que os dados recebidos são strings
-    this.cleanUp();
-    if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
 
-    if (this.body.password.length < 3 || this.body.password.length >= 50) {
-      this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
+  valida() {
+    this.cleanUp();
+
+    // Validação
+    // O e-mail precisa ser válido
+    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+
+    // A senha precisa ter entre 3 e 50
+    if(this.body.password.length < 3 || this.body.password.length > 50) {
+      this.errors.push('A senha precisa ter entre 3 e 50 caracteres.');
     }
   }
 
   cleanUp() {
-    for (const key in this.body) {
-      if (typeof this.body[key] !== 'string') {
+    for(const key in this.body) {
+      if(typeof this.body[key] !== 'string') {
         this.body[key] = '';
       }
     }
-    //podem vir mais campos que queremos no forms
-    //para garantir que teremos apenas os campos que queremos fazemos o seguinte:
+
     this.body = {
       email: this.body.email,
       password: this.body.password
