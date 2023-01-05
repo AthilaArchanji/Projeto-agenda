1ab8f94149b0ebc29e288ddfde18c1f763d819a8
Athila <athila.rodriguess@gmail.com>
2023-01-05 11:54:44 -0300

    logando usuario :fire:

--- routes.js
+++ routes.js
@@ -8,8 +8,9 @@ const loginController = require('./src/controllers/loginController');
 route.get('/', homeController.index);
 
 // Rotas de login
-route.get('/login', loginController.index);
+route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
 
 
 module.exports = route;
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -10,51 +10,62 @@ const LoginSchema = new mongoose.Schema({
 const LoginModel = mongoose.model('Login', LoginSchema);
 
 class Login {
-  constructor(body){
+  constructor(body) {
     this.body = body;
     this.errors = [];
     this.user = null;
   }
 
-  async register(){
+  async login() {
     this.valida();
     if(this.errors.length > 0) return;
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if(!this.user) {
+      this.errors.push('Usuário não existe.');
+      return;
+    }
+    if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push('Senha inválida');
+      this.user = null;
+      return;
+    }
+  }
+
+  async register() {
+    this.valida();
+    if (this.errors.length > 0) return;
 
     //Impedindo de cadastrar mais de um usuario
     await this.userExists();
 
-    if(this.errors.length > 0) return;
+    if (this.errors.length > 0) return;
 
     //Encriptando a senha do usuario
     const salt = bcryptjs.genSaltSync();
     this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-    try{
-      this.user = await LoginModel.create(this.body);
-    }
-    catch(e){
-      console.log(e)
-    }
+    this.user = await LoginModel.create(this.body);
   }
-  async userExists(){
-    const user = await LoginModel.findOne({email: this.body.email});
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email });
 
-    if(user) this.errors.push('Usuário já existe.');
+    if (user) this.errors.push('Usuário já existe.');
   }
-  valida(){
+  valida() {
     //Primeiro chamamos a função cleanUp() para limpar o campo 
     //e termos certeza que os dados recebidos são strings
     this.cleanUp();
-    if(!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
+    if (!validator.isEmail(this.body.email)) this.errors.push('E-mail inválido');
 
-    if(this.body.password.length < 3 || this.body.password.length >= 50){
+    if (this.body.password.length < 3 || this.body.password.length >= 50) {
       this.errors.push('A senha precisa ter entre 3 e 50 caracteres');
     }
   }
 
-  cleanUp(){
-    for(const key in this.body) {
-      if(typeof this.body[key] !== 'string') {
+  cleanUp() {
+    for (const key in this.body) {
+      if (typeof this.body[key] !== 'string') {
         this.body[key] = '';
       }
     }
